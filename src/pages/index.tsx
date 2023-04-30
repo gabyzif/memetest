ace3ee79a07c0844499a744487d86d7bf07a8efa
gabyzif <gabyzif@gmail.com>
2023-04-30 12:52:54 -0300

    update index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -23,12 +23,13 @@ export default function Home({ categories }) {
           <Container width="80%" height="auto">
             <div className="w-full py-10 ">
               <h1 className=" text-5xl mb-3 tracking-wide font-bold uppercase font-play">Memetest</h1>
-              <p className=" font-thin">Choose a category!</p>
+              <p className=" font-thin">
+                Choose a category to start a new game or continue your previous session
+              </p>
             </div>
 
             {categories ? <ListButtons categories={categories} /> : <p>Loading...</p>}
             <div className="flex justify-center my-3 gap-3">
-              <Button text="START" href="#" variant="secondary" />
               <button onClick={() => continueGame()}> Continue </button>
             </div>
           </Container>
