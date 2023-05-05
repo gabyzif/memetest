d96b7ea4123e8a4cf2239f39842bd8ebff858e29
gabyzif <gabyzif@gmail.com>
2023-05-05 14:41:57 -0300

    update modal's styles

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -8,12 +8,12 @@ interface IModal {
 const Modal: React.FC<IModal> = ({ score, onClose }) => {
   return (
     <div className=" w-screen h-screen  absolute bg-gray-800 m-0 top-0 left-0 flex bg-opacity-90	">
-      <Container variant="primary" className=" w-6/12  shadow-lg shadow-lime-300  h-40 md:h-1/4">
+      <div className=" md:w-4/12 bg-primary-light w-9/12 justify-center h-fit shadow-lg shadow-lime-300 m-auto p-10 rounded-xl ">
         <div className="grid ">
           <button className="justify-self-end" onClick={onClose}>
             <h1 className="text-gray-700">X</h1>
           </button>
-          <div className="w-full m-auto  h-60 md:h-10 text-center">
+          <div className="w-full m-auto  text-center">
             <h2 className="uppercase text-3xl mb-2 font-bold text-gray-700">Score: {score}</h2>
             <div className="m-auto w-full  flex  justify-center text-center">
               <Button variant="secondary" href="/">
@@ -22,7 +22,7 @@ const Modal: React.FC<IModal> = ({ score, onClose }) => {
             </div>
           </div>
         </div>
-      </Container>{' '}
+      </div>{' '}
     </div>
   );
 };
